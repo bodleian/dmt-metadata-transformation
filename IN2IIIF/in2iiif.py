411c39ccee3a770e353ba7a32b2f797ce0afd85c
Tanya Gray <tanya.gray@bodleian.ox.ac.uk>
2015-12-21 16:57:41 +0000

    add documentation, exception handling and additional methods

--- IN2IIIF/in2iiif.py
+++ IN2IIIF/in2iiif.py
@@ -3,114 +3,152 @@ import ConfigParser
 import ast
 import argparse
 
-  
+# Implemntation of Borg design pattern - an object-oriented way to provide global variables 
+
+
 
 class One:
-    _shared_state = {} # attribute dictionary
+    """One class (Borg design pattern) making class attributes global."""
+    
+    _shared_state = {} # Attribute dictionary
 
     def __init__(self):
-        self.__dict__ = self._shared_state
+        self.__dict__ = self._shared_state # Make it an attribute dictionary
         
         
-class GlobalConfig(One): # inherits from the GlobalConfig class
-    # this class shares all its attributes among its various instances  
+        
+class GlobalConfig(One): # inherits from the One class
+     """GlobalConfig class shares all its attributes among its various instances."""
+     # GlobalConfig objects are an object-oriented form of global variables
+     
      def __init__(self, **kwargs):
          One.__init__(self)
+         # Update the attribute dictionary by inserting a new key-value pair defined in argument
          self._shared_state.update(kwargs)
      
      def __setitem__(self, key, value):
+         # Update the attribute dictionary by inserting a new key-value pair
          self._shared_state[key] = value
          
      def __getitem__(self, key):
+         # Returns a value in the dictionary using a key
          value = self._shared_state[key]  
          return value   
          
      def __str__(self):
+         # Returns the attribute dictionary for printing
          return str(self._shared_state)
     
              
 class In2iiif():
-    
+    """Base class for transformation of various formats to IIIF."""
     def __init__(self, **kwargs):
-            
-         arg = GlobalConfig()
+         
+         arg = GlobalConfig() # instantiate GlobalConfig class to hold global variables
             
 
     def setFactoryProperties(self, fac):
-         arg = GlobalConfig()
+         """Sets the ManifestFactory Factory properties."""
+         arg = GlobalConfig() # instantiate GlobalConfig class to hold global variables
+         
+         # set attributes for ManifestFactory instance
          fac.set_base_metadata_uri(arg.manifest_base_metadata_uri) # Where the resources live on the web
          fac.set_base_metadata_dir(arg.manifest_base_metadata_dir) # Where the resources live on disk
-        
          fac.set_base_image_uri(arg.manifest_base_image_uri) # Default Image API information
          fac.set_iiif_image_info(arg.manifest_iiif_image_info_version,arg.manifest_iiif_image_info_compliance) # Version, ComplianceLevel
-        
          fac.set_debug(arg.debug) 
     
     def setManifestProperties(self, manifest):
+         """Sets the ManifestFactory Manifest's properties."""
+         # instance of global variables
          arg = GlobalConfig()
+         # set ManifestFactory manifest description and viewing direction using global variables
          manifest.description = arg.manifest_description
          manifest.viewingDirection = arg.manifest_viewingDirection
         
     def setCanvasProperties(self, cvs, img):
-            cvs.height = img.height
-            cvs.width = img.width
+         """Sets the ManifestFactory Canvas properties."""
+         cvs.height = img.height
+         cvs.width = img.width
     
     def setImageProperties(self, img, imagefile):
-            
-            img.set_hw_from_file(imagefile) 
-            # OR if you have a IIIF service:
-            #  img.set_hw_from_iiif()
+         """Sets the ManifestFactory Image properties."""   
+         img.set_hw_from_file(imagefile) 
+         # OR if you have a IIIF service:
+         #  img.set_hw_from_iiif()
     
     def parseConfig(self):
-        
-         arg = GlobalConfig() 
-               
-         config = ConfigParser.ConfigParser() # config file parser
-         config.read(arg.config) # read configuration file defined in command line arguments
+         """Parses configuration file and adds values to GlobalConfig attribute dictionary"""
          
-         GlobalConfig(   
-                manifest_base_metadata_uri = config.get('manifest','base_metadata_uri'),
-                manifest_base_metadata_dir = config.get('manifest','base_metadata_dir'),
-                manifest_base_image_uri = config.get('manifest','base_image_uri'),
-                manifest_iiif_image_info_version = config.get('manifest','iiif_image_info_version'),
-                manifest_iiif_image_info_compliance = config.get('manifest','iiif_image_info_compliance'),
-                debug = config.get('manifest','debug'),
-                manifest_id = config.get('manifest','id'),
-                manifest_label = config.get('manifest','label'),
-                manifest_description = config.get('manifest','description'),
-                manifest_viewingDirection = config.get('manifest','viewingDirection'),
-                sequence_id = config.get('sequence','id'),
-                sequence_label = config.get('sequence','label'),
-                canvas_id = config.get('canvas','id'),
-                canvas_label = config.get('canvas','label'),
-                annotation_id = config.get('annotation','id'),
-                metadata = '')
-                          
+         arg = GlobalConfig() # instance of GlobalConfig        
+         config = ConfigParser.ConfigParser() # instance of config file parser
+         
+         try:
+             config.read(arg.config) # read configuration file defined in command line arguments
+         
+             # pass values to instance of GlobalConfig as arguments, that will add them to dictionary
+             GlobalConfig(   
+                    manifest_base_metadata_uri = config.get('manifest','base_metadata_uri'),
+                    manifest_base_metadata_dir = config.get('manifest','base_metadata_dir'),
+                    manifest_base_image_uri = config.get('manifest','base_image_uri'),
+                    manifest_iiif_image_info_version = config.get('manifest','iiif_image_info_version'),
+                    manifest_iiif_image_info_compliance = config.get('manifest','iiif_image_info_compliance'),
+                    debug = config.get('manifest','debug'),
+                    manifest_id = config.get('manifest','id'),
+                    manifest_label = config.get('manifest','label'),
+                    manifest_description = config.get('manifest','description'),
+                    manifest_viewingDirection = config.get('manifest','viewingDirection'),
+                    sequence_id = config.get('sequence','id'),
+                    sequence_label = config.get('sequence','label'),
+                    canvas_id = config.get('canvas','id'),
+                    canvas_label = config.get('canvas','label'),
+                    annotation_id = config.get('annotation','id'),
+                    metadata = '')
+         
+         except IOError as e:
+             print('Cannot read configuration file: ', e)
+         
+         except Exception as e:
+             print('Problem encountered adding values in the configuration file to the global dictionary: ', e)
+                              
         
     def parseArguments(self):
-        
-         parser = argparse.ArgumentParser() # command line argument parser
+         """Parses command line arguments and adds values to GlobalConfig attribute dictionary"""
+         
+         parser = argparse.ArgumentParser() # instance of command line argument parser
 
-         #positional arguments
-         parser.add_argument("config", help="configuration file")
-         parser.add_argument("input", help="input source file")
-         # manifest factory doesen't appear to allow output file location to be specified
-       #  parser.add_argument("output",
-       #              help="output destination file")
-         parser.add_argument("image_src", help="image source: directory | mets_file")
+         #POSITIONAL/REQUIRED ARGUMENTS
+         parser.add_argument("--config", help="configuration file", required="True")
+         parser.add_argument("--input",  help="input source file", required="True")
+         
+         # requirement for software was to allow output directory to be defined, however,
+         # manifest factory doesen't appear to allow output file location to be specified   
+         # parser.add_argument("output", help="output destination file")
+         
+         parser.add_argument("--image_src",  help="image source: directory | mets_file", required="True")
 
-         #optional arguments
+         #OPTIONAL ARGUMENTS
+         
+         # eg /home/dmt/Documents/IIIF_Ingest/images/
          parser.add_argument("--image_dir", help="location of image directory (if source folder used for images")
-# eg /home/dmt/Documents/IIIF_Ingest/images/
-
          parser.add_argument("--compact", help="Should json be compact form or human-readable: Compact | Normal")
 
-         arg = parser.parse_args()
-         
-         GlobalConfig(config = arg.config )
-         GlobalConfig(input = arg.input)
-        # GlobalConfig(output = arg.output )
-         GlobalConfig(image_src = arg.image_src )
-         GlobalConfig(image_dir = arg.image_dir )
-         GlobalConfig(compact = arg.compact)
+         # convert argument strings to objects and assign them as attributes
+         # https://docs.python.org/2/library/argparse.html#the-parse-args-method
+         try:
+             arguments = parser.parse_args()
+         except Exception as e:
+             print('Problem encountered with reading command line arguments: ', e)
+             
+         # Add command line arguments to the global dictionary using GlobalConfig
+         try:
+             GlobalConfig(config = arguments.config )
+             GlobalConfig(input = arguments.input)
+             GlobalConfig(image_src = arguments.image_src )
+             GlobalConfig(image_dir = arguments.image_dir )
+             GlobalConfig(compact = arguments.compact)
+             # GlobalConfig(output = arguments.output ) 
+         except Exception as e:
+             print('Problem encountered with adding command line arguments to global variables: ', e)
+                 
   
--- IN2IIIF/mets2iiif.py
+++ IN2IIIF/mets2iiif.py
@@ -12,97 +12,244 @@ execfile('in2iiif.py')
 
 
 class Mets2iiif(In2iiif):
-    
+     """Transforms METS to IIIF using ManifestFactory and in2iiif.""" 
+     
      def __init__(self, **kwargs):
+         """Gets attributes from command line and config file and creates IIIF manifest using ManifestFactory."""
+          
+         self.initSettings() # read config file and command line args and add to global vars
+       
+         factory = self.initFactory() # create ManifestFactory factory
          
-         arg = GlobalConfig()
+         manifest = self.initManifest(factory) # define manifest
          
-         self.parseArguments()   
-         self.parseConfig()
-         self.parseConfigMetadata()
+         sequence = self.initSequence(manifest) # define sequence
          
-        # factory
-         fac = ManifestFactory()
-         self.setFactoryProperties(fac)
+         structMap = self.getMetsFileStructMap()  # read METS file structMap
+         
+         self.initCanvas(sequence, structMap) # define canvases, annotations and images
+         
+         self.outputManifest(manifest) # write to file
+          
+          
+     def initSettings(self):
+         """Reads command line arguments and config file and adds to global variables"""
+         
+         self.parseArguments()   # parse command line arguments and add to global variables
         
-        # manifest
-         manifest = fac.manifest(
-                                 ident=arg.manifest_id, 
-                                 label= arg.manifest_label
-                                 )
-         self.setManifestProperties(manifest)
-         self.setMetadata(manifest)
+         self.parseConfig()      # parse config file and add to global variables
          
-        # sequence
-        # assumption - 1 sequence
-         seq = manifest.sequence()
+         self.parseConfigMetadata() # parse config file and add metadata settings to global variables
+             
+               
+     def initFactory(self):
+         """Initalizes manifest factory and set properties"""
+         factory = ManifestFactory() # instance of ManifestFactory
+         
+         self.setFactoryProperties(factory) # set properties of ManifestFactory using global variables
+          
+         return factory    
+             
+             
+     def initManifest(self, factory):    
+         """Initializes manifest and sets manifest properties"""
+         
+         arg = GlobalConfig() # instance of GlobalConfig to hold global variables
+         
+         # set properties of manifest
+         manifest = factory.manifest( ident=arg.manifest_id, label= arg.manifest_label)
+         
+         self.setManifestProperties(manifest) # set ManifestFactory manifest properties using global vars
+         self.setMetadata(manifest) # set ManifestFactory manifest metadata block using global vars
+               
+         return manifest      
+               
+     def setMetadata(self, manifest):
+         """Sets metadata block of manifest."""
+         
+         arg = GlobalConfig() # global variables
+         
+         metadata = arg.metadata # get a string representation of a list of metadata properties from global variables
+         properties = metadata.split('|') # create array from string
+         dictMetadata = {} # create dictionary to hold values retrieved from mets file
+        
+         try:
+             file = open(arg.input, 'r')   # read mets file
+         except IOError as e:
+             print("Problem encountered with opening the METS file", e)
+         
+         try:        
+             doc = etree.parse(file)
+         except Exception as e:
+             print("Problem encountered with parsing the METS file", e)    
+         #file.close()
+
+         # iterate through array of metadata properties defined in the config file
+         # extract values from the METS file using xpath
+         for property in properties:
+             label = property
+             if label != '':
+                 key = 'metadata_' + property
+                 xpath = arg[key]  # get xpath for property
+                 
+                 if xpath != "":
+                     # use xpath to extract corresponding value from the mets file
+                     result = doc.xpath(xpath , namespaces={'mets': 'http://www.loc.gov/METS/', "mods": "http://www.loc.gov/mods/v3"})
+                     
+                     if result:
+                         if type(result) == str:
+                            value = result.strip()
+                         else:
+                             value = result[0].strip() 
+                         if value != "":     
+                            dictMetadata[label] = value
+            
+         # set metadata block for manifest if values have been extracted from the METS file
+         if bool(dictMetadata):
+             manifest.set_metadata(dictMetadata)      
+          
+          
+     def initSequence(self, manifest):
+         """Defines sequence for the manifest."""
+         
+        # assumption is that there is one sequence per manifest
+        
+         arg = GlobalConfig() # instance of GlobalConfig to hold global variables
+        
+         sequence = manifest.sequence()
+         
+         # if sequence_id defined in global variables use this
          if arg.sequence_id != "":
-             seq.id = arg.sequence_id
+             sequence.id = arg.sequence_id
+         # if seqeunce label defined in global variables use this    
          if arg.sequence_label != "":
-            seq.label = arg.sequence_label
-              
-         file = open(arg.input, 'r') # open mets file for reading
-         doc = etree.parse(file) # read file into etree for xpath queries
-         file.close() # close mets file
+            sequence.label = arg.sequence_label     
          
-         xpathOrderedList = "//mets:structMap/mets:div/mets:div"
-         orderedList = doc.xpath(xpathOrderedList, namespaces={'mets': 'http://www.loc.gov/METS/'})
+         return sequence   
+
+
+     def outputManifest(self, manifest):
+         """Write IIIF manifest to file."""
          
-         p = 0 # counter for canvas label and id
+         arg = GlobalConfig() # instance of GlobalConfig to hold global variables
          
-        # canvases
-        
-         for canvas in orderedList: # Create a canvas with uri slug of page-1, and label of Page 1
-            p += 1
-           
-            if canvas.attrib['ID']:
-                canvas_id = canvas.attrib['ID']
-            else:
-                canvas_id = arg.canvas_id +"-"+str(p) # use canvas id prefix defined in config
-            
-            if canvas.attrib['ORDERLABEL']:
-                 canvas_label = canvas.attrib['ORDERLABEL']
-            else:
-                canvas_label = arg.canvas_label+" " + str(p) # use canvas label prefix defined in config
-        
-            # determine whether to get images from local directory or via mets 
-            # use arg image_src directory mets_file and image_dir 
-            
-            if arg.image_src == 'directory':
-                # image information is from images in directory specified by image_dir
-                imageLocation = arg.image_dir
-            else:
-                # image information derived from mets file    
-                # get item id to retrieve file name
-                itemId = canvas.xpath('mets:fptr[1]/@FILEID', namespaces={'mets': 'http://www.loc.gov/METS/'})
-                xpathImageLocation = "//mets:fileSec//mets:file[@ID='" + itemId[0] +"']/mets:FLocat/@xlink:href"
-                imageLocation = doc.xpath(xpathImageLocation, namespaces={'mets': 'http://www.loc.gov/METS/', 'xlink':'http://www.w3.org/1999/xlink'})
-                imageLocation = imageLocation[0]
-            
-            cvs = seq.canvas(ident = canvas_id, label = canvas_label)
-        
-            # annotation
-            # assumption - 1 image per canvas
-            anno = cvs.annotation()
-            if arg.annotation_id != "":
-                anno.id = arg.annotation_id + canvas_id
-        
-            # image
-            img = anno.image("p%s" % p, iiif=True)
-        
-            self.setImageProperties(img, imageLocation)
-            self.setCanvasProperties(cvs, img)
-        
-        # should json output be compact?
+         # should json output be compact?
          if arg.compact == 'True':
              manifest.toFile()
          else:
              manifest.toFile(compact=False)
 
+
+     def initCanvas(self, sequence, structMap):
+         """Define canvases for manifest using METS file structMap."""
+         arg = GlobalConfig() # instance of GlobalConfig to hold global variables
+         
+         counter = 0 # counter for canvas label and id
+         
+        # create canvases using structMap
+         for item in structMap: 
+            counter += 1
+           
+            if item.attrib['ID']:
+                canvas_id = item.attrib['ID'] # set canvas id to id attribute
+            else:
+                canvas_id = arg.canvas_id +"-" + str(counter) # or if not present, use canvas id prefix defined in config
+            
+            if item.attrib['ORDERLABEL']:
+                 canvas_label = item.attrib['ORDERLABEL'] # set canvas lablel to orderlabel attribute
+            else:
+                canvas_label = arg.canvas_label+" " + str(counter) # or if not present, use canvas label prefix defined in config
+        
+            # canvas
+            canvas = sequence.canvas(ident = canvas_id, label = canvas_label)
+            
+            # annotation
+            annotation = self.initAnnotation(canvas, canvas_id)
+            
+            self.initImage(canvas, annotation, item, counter)
+            
+            
+            
+        
+        
+     def initAnnotation(self, canvas, canvas_id):
+         """Define annotation for canvas."""
+         arg = GlobalConfig() # instance of GlobalConfig to hold global variables
+         # annotation
+         
+         annotation = canvas.annotation()
+        
+         if arg.annotation_id != "":
+             annotation.id = arg.annotation_id + canvas_id
+
+         return annotation        
+     
+     
+     def initImage(self, canvas, annotation, item, counter):    
+         """Define image for annotation."""
+         
+         arg = GlobalConfig() # instance of GlobalConfig to hold global variables
+            
+         image_location = self.getImageLocation(item)
+         
+         # image - assumption - one image per canvas
+         image = annotation.image("p%s" % counter, iiif=True)
+        
+        
+         self.setImageProperties(image, image_location)
+        
+         self.setCanvasProperties(canvas, image)
+        
+                 
+     def getImageLocation(self, item):
+         """Determine image location."""
+         
+         arg = GlobalConfig() # instance of GlobalConfig to hold global variables
+         image_location = ""
+         
+         # determine whether to get images from local directory or via mets 
+            # use arg image_src directory mets_file and image_dir 
+            
+         if arg.image_src == 'directory':
+                # image information is from images in directory specified by image_dir
+               image_location = arg.image_dir
+         else:
+                # image information derived from mets file           
+                # get item id to retrieve file name
+                item_id = item.xpath('mets:fptr[1]/@FILEID', namespaces={'mets': 'http://www.loc.gov/METS/'})
+                xpathImageLocation = "//mets:fileSec//mets:file[@ID='" + item_id[0] +"']/mets:FLocat/@xlink:href"
+                image_location = doc.xpath(xpathImageLocation, namespaces={'mets': 'http://www.loc.gov/METS/', 'xlink':'http://www.w3.org/1999/xlink'})
+                image_location = image_location[0]
+                
+         return image_location        
+
+
+     def getMetsFileStructMap(self):
+         """Gets the structMap section of a METS file."""
+         arg = GlobalConfig() # instance of GlobalConfig to hold global variables
+         file = open(arg.input, 'r') # open mets file for reading
+         doc = etree.parse(file) # read file into etree for xpath queries
+         file.close() # close mets file
+         
+         xpath = "//mets:structMap/mets:div/mets:div"
+         xml = doc.xpath(xpath, namespaces={'mets': 'http://www.loc.gov/METS/'})
+         
+         return xml
+         
+        
+        
+        
+
+    
+
+    
+     
+     
+     
     
     
      def parseConfigMetadata(self):
-         
+         """"""
          arg = GlobalConfig()
          config = ConfigParser.ConfigParser() # config file parser
          config.read(arg.config) # read configuration file defined in command line arguments
@@ -114,42 +261,8 @@ class Mets2iiif(In2iiif):
              arg['metadata'] = arg.metadata + '|' + str(property)
          
          
-     def setMetadata(self, manifest):
-         
-         arg = GlobalConfig()
-         metadata = arg.metadata
-         properties = metadata.split('|')
-         dictMetadata = {}
-        
-        # read mets file
-        
-         file = open(arg.input, 'r')
-         doc = etree.parse(file)
-         #file.close()
-
-         for property in properties:
-             label = property
-             if label != '':
-                 key = 'metadata_' + property
-                 xpath = arg[key]  
-                 
-                 if xpath != "":
-                     
-                     result = doc.xpath(xpath , namespaces={'mets': 'http://www.loc.gov/METS/', "mods": "http://www.loc.gov/mods/v3"})
-                     
-                     if result:
-                         if type(result) == str:
-                            value = result.strip()
-                         else:
-                             value = result[0].strip() 
-                         if value != "":     
-                            dictMetadata[label] = value
-            
-                
-          
-                 
-         manifest.set_metadata(dictMetadata)
+    
 
 
-In2iiif()
-Mets2iiif()   
+In2iiif() # create a new instance of In2iiif class
+Mets2iiif()   # create a new instance of Mets2iiif class
